581921a647bad60509704c0413f58f57e838558b
Victoraluno1 <rm95110@estudante.fieb.edu.br>
2025-03-28 11:49:58 -0300

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,55 @@
 import { useState } from "react"
-
+ 
 function App(){
-
+ 
   const [nome, setNome] = useState('Ronaldo')
-
-  const Formulario = ()=>{
+  const [idade, setIdade] = useState(18)
+  const [time, setTime] = useState('Santos FC')
+  const [pi] = useState(3.14)
+ 
+  //var idade = 18;
+  //let time = 'Santos FC';
+  //const pi = 3.14;
+ 
+  const Formulario = (props)=> {
+ const [valor, setValor] = useState()
+    
     return (
       <div>
-<input 
-  className= "nome"
-  onChange={(e)=>{setNome(e.target.value)}}
-  type="text" />
-
-    <button
-    className="botao"
-    onClick={()=>{alert(nome)}}
-    >
-      Clique Aqui
-    </button>
+        <input
+          className = "nome"
+          placeholder= {props.sombra}
+          onChange={(e)=>{setValor(e.target.value)}}
+          type="text" />
+ 
+        <button
+          className="botao"
+          onClick={()=>{
+            if(valor.length > 0)
+              alert(valor);
+            else
+            alert(props.nome + ' idade é ' + props.idade)}}            
+        >
+          CLIQUE AQUI
+        </button>
       </div>
     )
   }
-
+ 
+ 
   return (
-<div>
-  <h3>Pizzaria 2E</h3>
-
- <Formulario /> 
- <Formulario />  
- <Formulario /> 
- <Formulario /> 
-</div>
+    <div>
+      <h3>Pizzaria 2E</h3>
+ 
+      <Formulario nome="Ricardo" 
+      idade={45}
+      sombra="digite nome..."/>
+      <Formulario nome="Maria" sombra="digite sua idade..."/>
+      <Formulario nome="Tomas" sombra="digite seu time..."/>
+      <Formulario nome="Antonieta" sombra="valor de pi"/>
+ 
+    </div>
   )
 }
-
+ 
 export default App
